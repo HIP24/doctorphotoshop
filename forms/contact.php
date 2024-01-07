1becc2babd71c01e8a0a4f93341f8f11d69a9170
HIP24 <halilpamuk24@gmail.com>
2024-01-07 01:56:53 +0100

    slider

--- forms/contact.php
+++ forms/contact.php
@@ -1,21 +1,38 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $to = 'lible1951@teleworm.us';
-  $subject = $_POST['subject'];
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  
-  $headers = "From: $name <$email>\r\n";
-  $headers .= "Reply-To: $name <$email>\r\n";
-  $headers .= "Content-type: text/plain; charset=utf-8\r\n";
-  
-  if(mail($to, $subject, $message, $headers)) {
-    echo 'success';
-  } else {
-    echo 'error';
-  }
+ini_set("SMTP", "mail.example.com");
+ini_set("smtp_port", "25");
+
+
+// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['subject']) || empty($_POST['message'])) {
+  http_response_code(400);
+  echo "Please fill out all required fields.";
+  exit;
 }
 
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Create email body
+$body = "Name: $name\nEmail: $email\n\nSubject: $subject\n\nMessage:\n$message";
+
+// Set up email headers
+$headers = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+// Send email
+$to = "lible1951@teleworm.us"; // Replace with your own email address
+if(mail($to, $subject, $body, $headers)) {
+  http_response_code(200);
+  echo "Thank you! Your message has been sent.";
+} else {
+  http_response_code(500);
+  echo "Oops! Something went wrong and we couldn't send your message.";
+}
 ?>
